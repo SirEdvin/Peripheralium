775f29eb4078270bdfdcbad9f13c142e6bf61862
SirEdvin <siredvin.dark@gmail.com>
2023-05-02 21:19:32 +0300

    feat: add item query logic instead of just id

--- projects/core/src/main/kotlin/site/siredvin/peripheralium/extra/plugins/AbstractItemStoragePlugin.kt
+++ projects/core/src/main/kotlin/site/siredvin/peripheralium/extra/plugins/AbstractItemStoragePlugin.kt
@@ -5,16 +5,13 @@ import dan200.computercraft.api.lua.LuaException
 import dan200.computercraft.api.lua.LuaFunction
 import dan200.computercraft.api.peripheral.IComputerAccess
 import dan200.computercraft.api.peripheral.IPeripheral
-import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
 import net.minecraft.world.level.Level
 import site.siredvin.peripheralium.api.peripheral.IPeripheralPlugin
 import site.siredvin.peripheralium.api.storage.ExtractorProxy
 import site.siredvin.peripheralium.api.storage.Storage
 import site.siredvin.peripheralium.api.storage.StorageUtils
 import site.siredvin.peripheralium.common.configuration.PeripheraliumConfig
-import site.siredvin.peripheralium.xplat.XplatRegistries
 import java.util.*
 import java.util.function.Predicate
 import kotlin.math.min
@@ -37,41 +34,27 @@ abstract class AbstractItemStoragePlugin: IPeripheralPlugin {
     }
 
     @LuaFunction(mainThread = true)
-    fun pushItem(computer: IComputerAccess, toName: String, itemName: Optional<String>, limit: Optional<Int>): Int {
+    fun pushItem(computer: IComputerAccess, toName: String, itemQuery: Optional<Any>, limit: Optional<Int>): Int {
         val location: IPeripheral = computer.getAvailablePeripheral(toName)
             ?: throw LuaException("Target '$toName' does not exist")
 
         val toStorage = ExtractorProxy.extractTargetableStorage(level, location.target)
             ?: throw LuaException("Target '$toName' is not an targetable storage")
 
-        val predicate: Predicate<ItemStack> = if (itemName.isEmpty) {
-            Predicate { true }
-        } else {
-            val item = XplatRegistries.ITEMS.get(ResourceLocation(itemName.get()))
-            if (item == Items.AIR)
-                throw LuaException("There is no item ${itemName.get()}")
-            Predicate { it.`is`(item) }
-        }
+        val predicate: Predicate<ItemStack> = PeripheralPluginUtils.itemQueryToPredicate(itemQuery)
         val realLimit = min(PeripheraliumConfig.itemStorageTransferLimit, limit.orElse(Int.MAX_VALUE))
         return StorageUtils.moveBetweenStorages(storage, toStorage, realLimit, takePredicate = predicate)
     }
 
     @LuaFunction(mainThread = true)
-    fun pullItem(computer: IComputerAccess, fromName: String, itemName: Optional<String>, limit: Optional<Int>): Int {
+    fun pullItem(computer: IComputerAccess, fromName: String, itemQuery: Optional<Any>, limit: Optional<Int>): Int {
         val location: IPeripheral = computer.getAvailablePeripheral(fromName)
             ?: throw LuaException("Target '$fromName' does not exist")
 
         val fromStorage = ExtractorProxy.extractStorage(level, location.target)
             ?: throw LuaException("Target '$fromName' is not an storage")
 
-        val predicate: Predicate<ItemStack> = if (itemName.isEmpty) {
-            Predicate { true }
-        } else {
-            val item = XplatRegistries.ITEMS.get(ResourceLocation(itemName.get()))
-            if (item == Items.AIR)
-                throw LuaException("There is no item ${itemName.get()}")
-            Predicate { it.`is`(item) }
-        }
+        val predicate: Predicate<ItemStack> = PeripheralPluginUtils.itemQueryToPredicate(itemQuery)
         val realLimit = min(PeripheraliumConfig.itemStorageTransferLimit, limit.orElse(Int.MAX_VALUE))
         return StorageUtils.moveBetweenStorages(fromStorage, storage, realLimit, takePredicate = predicate)
     }
--- projects/core/src/main/kotlin/site/siredvin/peripheralium/extra/plugins/PeripheralPluginUtils.kt
+++ projects/core/src/main/kotlin/site/siredvin/peripheralium/extra/plugins/PeripheralPluginUtils.kt
@@ -1,10 +1,60 @@
 package site.siredvin.peripheralium.extra.plugins
 
+import dan200.computercraft.api.lua.LuaException
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import site.siredvin.peripheralium.xplat.XplatRegistries
+import java.util.*
+import java.util.function.Predicate
+
 object PeripheralPluginUtils {
     object TYPES {
         const val INVENTORY = "inventory"
         const val FLUID_STORAGE = "fluid_storage"
         const val ITEM_STORAGE = "item_storage"
         const val ENERGY_STORAGE = "energy_storage"
+    }
+
+    private object ITEM_QUERY_FIELD {
+        const val name = "name"
+        const val displayName = "displayName"
+        const val tag = "tag"
+    }
+
+    private val ALWAYS_ITEM_STACK_TRUE: Predicate<ItemStack> = Predicate { true }
+
+    fun builtItemNamePredicate(name: String): Predicate<ItemStack> {
+        val item = XplatRegistries.ITEMS.get(ResourceLocation(name))
+        if (item == Items.AIR)
+            throw LuaException("There is no item $name")
+        return Predicate { it.`is`(item) }
+    }
+
+    fun builtItemDisplayNamePredicate(displayName: String): Predicate<ItemStack> {
+        return Predicate { it.hoverName.string == displayName }
+    }
+
+    fun builtItemTagPredicate(tag: String): Predicate<ItemStack> {
+        return Predicate { itemStack -> itemStack.tags.anyMatch{ it.location.toString() == tag } }
+    }
+
+    fun itemQueryToPredicate(optSomething: Optional<Any>): Predicate<ItemStack> {
+        if (optSomething.isEmpty)
+            return ALWAYS_ITEM_STACK_TRUE
+        val something = optSomething.get()
+        if (something is String) {
+            return builtItemNamePredicate(something)
+        } else if (something is Map<*, *>) {
+            var aggregated_predicate = ALWAYS_ITEM_STACK_TRUE
+            if (something.contains(ITEM_QUERY_FIELD.name))
+                aggregated_predicate = aggregated_predicate.and(builtItemNamePredicate(something[ITEM_QUERY_FIELD.name].toString()))
+            if (something.contains(ITEM_QUERY_FIELD.displayName))
+                aggregated_predicate = aggregated_predicate.and(builtItemDisplayNamePredicate(something[ITEM_QUERY_FIELD.displayName].toString()))
+            if (something.contains(ITEM_QUERY_FIELD.tag))
+                aggregated_predicate = aggregated_predicate.and(builtItemTagPredicate(something[ITEM_QUERY_FIELD.tag].toString()))
+            return aggregated_predicate
+        }
+        throw LuaException("Item query should be string or table")
     }
 }
