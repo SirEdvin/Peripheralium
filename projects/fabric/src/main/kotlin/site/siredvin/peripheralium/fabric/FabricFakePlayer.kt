b8842d4a4c29d295cc5f20f99a1747959a3deb59
SirEdvin <siredvin.dark@gmail.com>
2023-06-03 12:03:57 +0300

    fix(fabric): fake player logic

--- projects/fabric/src/main/kotlin/site/siredvin/peripheralium/fabric/FabricFakePlayer.kt
+++ projects/fabric/src/main/kotlin/site/siredvin/peripheralium/fabric/FabricFakePlayer.kt
@@ -1,42 +1,21 @@
 package site.siredvin.peripheralium.fabric
 
 import com.mojang.authlib.GameProfile
+import net.fabricmc.fabric.api.entity.FakePlayer
 import net.minecraft.server.level.ServerLevel
-import net.minecraft.server.level.ServerPlayer
-import net.minecraft.stats.Stat
-import net.minecraft.world.MenuProvider
 import net.minecraft.world.damagesource.DamageSource
-import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityDimensions
 import net.minecraft.world.entity.Pose
 import net.minecraft.world.entity.player.Player
-import java.util.*
 
 class FabricFakePlayer(serverLevel: ServerLevel, gameProfile: GameProfile) :
-    ServerPlayer(serverLevel.server, serverLevel, gameProfile) {
-    init {
-        connection = FakeNetHandler(this)
-    }
-
-    override fun tick() {}
-
-    override fun awardStat(stat: Stat<*>, increment: Int) {}
-    override fun isInvulnerableTo(source: DamageSource): Boolean {
-        return true
-    }
+    FakePlayer(serverLevel, gameProfile) {
 
     override fun canHarmPlayer(other: Player): Boolean {
         return true
     }
 
     override fun die(damageSource: DamageSource) {}
-    override fun openMenu(menu: MenuProvider?): OptionalInt {
-        return OptionalInt.empty()
-    }
-
-    override fun startRiding(vehicle: Entity, force: Boolean): Boolean {
-        return false
-    }
 
     override fun getEyeY(): Double {
         // Override this to make eye position correspond turtle eyes
@@ -45,6 +24,10 @@ class FabricFakePlayer(serverLevel: ServerLevel, gameProfile: GameProfile) :
 
     override fun getStandingEyeHeight(pose: Pose, dimensions: EntityDimensions): Float {
         return 0f
+    }
+
+    override fun getAttackStrengthScale(f: Float): Float {
+        return 1f
     }
 
     companion object {
