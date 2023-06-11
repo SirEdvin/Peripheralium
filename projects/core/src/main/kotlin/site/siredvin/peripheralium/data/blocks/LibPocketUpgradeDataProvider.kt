b782fa07e1f1a70d504824940faeac9b975f920d
SirEdvin <siredvin.dark@gmail.com>
2023-06-11 20:55:18 +0300

    feat(data): from object to supplier

--- projects/core/src/main/kotlin/site/siredvin/peripheralium/data/blocks/LibPocketUpgradeDataProvider.kt
+++ projects/core/src/main/kotlin/site/siredvin/peripheralium/data/blocks/LibPocketUpgradeDataProvider.kt
@@ -6,9 +6,12 @@ import net.minecraft.data.PackOutput
 import net.minecraft.resources.ResourceLocation
 import site.siredvin.peripheralium.xplat.XplatRegistries
 import java.util.function.Consumer
+import java.util.function.Supplier
 
-abstract class LibPocketUpgradeDataProvider(output: PackOutput, serializers: List<PocketUpgradeSerialiser<*>>) : PocketUpgradeDataProvider(output) {
-    private val serializers: List<ResourceLocation> = serializers.map(XplatRegistries.POCKET_SERIALIZERS::getKey)
+abstract class LibPocketUpgradeDataProvider(output: PackOutput, serializers: List<Supplier<PocketUpgradeSerialiser<*>>>) : PocketUpgradeDataProvider(output) {
+    private val serializers: List<ResourceLocation> = serializers.map {
+        XplatRegistries.POCKET_SERIALIZERS.getKey(it.get())
+    }
     override fun addUpgrades(addUpgrade: Consumer<Upgrade<PocketUpgradeSerialiser<*>>>) {
         val touchedSerializers = mutableSetOf<ResourceLocation>()
         registerUpgrades {
--- projects/core/src/main/kotlin/site/siredvin/peripheralium/data/blocks/LibTurtleUpgradeDataProvider.kt
+++ projects/core/src/main/kotlin/site/siredvin/peripheralium/data/blocks/LibTurtleUpgradeDataProvider.kt
@@ -6,9 +6,12 @@ import net.minecraft.data.PackOutput
 import net.minecraft.resources.ResourceLocation
 import site.siredvin.peripheralium.xplat.XplatRegistries
 import java.util.function.Consumer
+import java.util.function.Supplier
 
-abstract class LibTurtleUpgradeDataProvider(output: PackOutput, serializers: List<TurtleUpgradeSerialiser<*>>) : TurtleUpgradeDataProvider(output) {
-    private val serializers: List<ResourceLocation> = serializers.map(XplatRegistries.TURTLE_SERIALIZERS::getKey)
+abstract class LibTurtleUpgradeDataProvider(output: PackOutput, serializers: List<Supplier<TurtleUpgradeSerialiser<*>>>) : TurtleUpgradeDataProvider(output) {
+    private val serializers: List<ResourceLocation> = serializers.map {
+        XplatRegistries.TURTLE_SERIALIZERS.getKey(it.get())
+    }
     override fun addUpgrades(addUpgrade: Consumer<Upgrade<TurtleUpgradeSerialiser<*>>>) {
         val touchedSerializers = mutableSetOf<ResourceLocation>()
         registerUpgrades {
