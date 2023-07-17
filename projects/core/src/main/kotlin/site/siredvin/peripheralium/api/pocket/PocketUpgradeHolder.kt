7196f95167c9f76bf7fbd8c73c498f3a6b97a5ce
SirEdvin <siredvin.dark@gmail.com>
2023-07-17 10:46:03 +0300

    feat(api): correct upgrade holder api

--- projects/core/src/main/kotlin/site/siredvin/peripheralium/api/pocket/PocketUpgradeHolder.kt
+++ projects/core/src/main/kotlin/site/siredvin/peripheralium/api/pocket/PocketUpgradeHolder.kt
@@ -1,8 +1,9 @@
 package site.siredvin.peripheralium.api.pocket
 
+import dan200.computercraft.api.pocket.IPocketAccess
 import dan200.computercraft.api.pocket.IPocketUpgrade
 import dan200.computercraft.api.upgrades.UpgradeData
 
 interface PocketUpgradeHolder {
-    fun getInternalUpgrades(): List<UpgradeData<IPocketUpgrade>>
+    fun getInternalUpgrades(pocket: IPocketAccess): List<UpgradeData<IPocketUpgrade>>
 }
--- projects/core/src/main/kotlin/site/siredvin/peripheralium/api/turtle/TurtleUpgradeHolder.kt
+++ projects/core/src/main/kotlin/site/siredvin/peripheralium/api/turtle/TurtleUpgradeHolder.kt
@@ -1,8 +1,10 @@
 package site.siredvin.peripheralium.api.turtle
 
+import dan200.computercraft.api.turtle.ITurtleAccess
 import dan200.computercraft.api.turtle.ITurtleUpgrade
+import dan200.computercraft.api.turtle.TurtleSide
 import dan200.computercraft.api.upgrades.UpgradeData
 
 interface TurtleUpgradeHolder {
-    fun getInternalUpgrades(): List<UpgradeData<ITurtleUpgrade>>
+    fun getInternalUpgrades(turtle: ITurtleAccess, side: TurtleSide): List<UpgradeData<ITurtleUpgrade>>
 }
