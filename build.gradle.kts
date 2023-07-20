753972fbc88499cedfed9745bd104f5bad5091b1
SirEdvin <siredvin.dark@gmail.com>
2023-07-20 21:56:42 +0300

    chore: update internal plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("site.siredvin.root") version "0.4.4"
-    id("site.siredvin.release") version "0.4.4"
+    id("site.siredvin.root") version "0.4.5"
+    id("site.siredvin.release") version "0.4.5"
 }
 
 subprojectShaking {
