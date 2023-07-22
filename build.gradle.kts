6be6d178609b77fcd5fabb8ac08a0d134b236504
SirEdvin <siredvin.dark@gmail.com>
2023-07-22 17:00:11 +0300

    chore: update modding env

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("site.siredvin.root") version "0.4.5"
-    id("site.siredvin.release") version "0.4.5"
+    id("site.siredvin.root") version "0.4.9"
+    id("site.siredvin.release") version "0.4.9"
 }
 
 subprojectShaking {
